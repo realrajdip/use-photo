c8a7d42ace258e6af6f0a475e426f80f1a4ac327
Rajdip Bandopadhyay <rajdipbandopadhyay100@gmail.com>
2024-07-15 18:40:19 +0530

    updated

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,17 +1,16 @@
 export interface UploadResult {
-    public_id: string;
-    secure_url: string;
-  }
-  
-  export interface UploadOptions {
-    folder?: string;
-    tags?: string[];
-  }
-  
-  export interface UseUploadResult {
-    upload: (file: File) => Promise<void>;
-    error: any;
-    uploading: boolean;
-    result: UploadResult | null;
-  }
-  
+  public_id: string;
+  secure_url: string;
+}
+
+export interface UploadOptions {
+  folder?: string;
+  tags?: string[];
+}
+
+export interface UseUploadResult {
+  upload: (file: File) => Promise<void>;
+  error: any;
+  uploading: boolean;
+  result: UploadResult | null;
+}
